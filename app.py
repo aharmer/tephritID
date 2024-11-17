538de1f522046c02a87ac525fcad1f0ebaa2c6c0
Aaron Harmer <HarmerA@landcareresearch.co.nz>
2024-11-18 12:07:57 +1300

    Added dark theme and italics

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0eb2325367770ad9eaf35f1011ad797079e0da4e504a975abc7d0c4c26a76119
-size 6695
+oid sha256:c8f4a45ee7fe28f0e5b220f6deabd291fdb3bf3dbf45468e0491b0815eed5f12
+size 6817
